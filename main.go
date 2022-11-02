7604bc1da9f93e77e664a14750335728c90beb63
Tejas Mane <tejasmane485@gmail.com>
2022-11-02 16:32:31 +0530

    hello handler added

--- main.go
+++ main.go
@@ -2,10 +2,32 @@ package main
 
 import (
 	"fmt"
-	// "log"
-	// "net/http"
+	"log"
+	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
+}
+
 func main() {
-	fmt.Println()
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
